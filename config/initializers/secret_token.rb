1ab751971ed8900220f26edf769568b80c1dd94e
Feo <jjzxcc@gmail.com>
2012-05-24 09:26:33 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ISpree::Application.config.secret_token = 'ce92a046aacaf7b99163db4e037b169e7b70ad9e5f755be76cd66b1de6c1d194094905f0191d6bea477a7b9d6ba5f3c9ce39c72bc1e69d129cbd2857f2e912f9'
